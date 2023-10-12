b18f230b5be1f7e8caf7bf231148c1a26a1cd7c3
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2023-10-12 14:25:28 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,6 +2,7 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
